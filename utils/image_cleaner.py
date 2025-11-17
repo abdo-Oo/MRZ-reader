841746466b53e1d879750da7a4a4266ac19cf9dc
abdo-Oo <82310791+abdo-Oo@users.noreply.github.com>
2025-11-17 16:31:37 +0200

    Update image_cleaner.py

--- utils/image_cleaner.py
+++ utils/image_cleaner.py
@@ -2,20 +2,10 @@ import numpy as np
 from PIL import Image
 
 def normalize_image(input_img):
-    """
-    Ensures the image is valid for PassportEye:
-    - Convert to PIL.Image
-    - Convert to RGB
-    - Return clean NumPy array (H, W, 3)
-    """
-    # If already numpy → convert to PIL
     if isinstance(input_img, np.ndarray):
         img = Image.fromarray(input_img)
     else:
         img = input_img
 
-    # Convert to RGB (removes alpha / grayscale)
     img = img.convert("RGB")
-
-    # Return RGB NumPy array
-    return np.asarray(img).copy()
+    return np.array(img).astype("uint8")
