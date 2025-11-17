b911a44622068be7323a61037fe3edf9fd4c6a1f
abdo-Oo <82310791+abdo-Oo@users.noreply.github.com>
2025-11-17 16:11:21 +0200

    Update mrz_extract.py

--- utils/mrz_extract.py
+++ utils/mrz_extract.py
@@ -1,1 +1,16 @@
+import cv2
+import numpy as np
+from passporteye import read_mrz
+from PIL import Image
 
+def extract_mrz_from_image(image):
+    """Try multiple rotations until MRZ is found."""
+    pil_img = Image.fromarray(image)
+    
+    for angle in [0, 90, 180, 270]:
+        rotated = np.array(pil_img.rotate(angle, expand=True))
+        mrz = read_mrz(rotated)
+        if mrz:
+            return mrz.to_dict(), rotated
+    
+    return None, image
